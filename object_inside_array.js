063585d97ba55a5058011dd4def158fad1b8a1c8
Mohit <nagarmohit138@gmail.com>
2023-09-11 16:51:24 +0530

    object inside array

---
+++ object_inside_array.js
@@ -0,0 +1,14 @@
+
+const array = [
+    {
+        user1:"Mohit",
+        age:21
+    },
+    {
+         user2 :"Deepak",
+         age:22
+    }
+]
+const [{user1:a1,age:b1},{user2:a2,age:b2}] = array
+console.log(a1 + " , " + b1)
+console.log(a2 + " , " + b2)
