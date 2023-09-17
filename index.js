37a1a222f55be34d133d040c59ef989c42801019
Mohit <nagarmohit138@gmail.com>
2023-09-17 16:08:01 +0530

    dom

---
+++ index.js
@@ -0,0 +1,21 @@
+//DOM - Document object model
+// console.dir(document)
+
+// const mainHeading = document.getElementById("main-heading");
+// const mainHeading = document.querySelector("#main-heading");
+// const header = document.querySelector(".header");
+// const navItem = document.querySelectorAll(".nav-item");
+// console.log(navItem);
+// console.log(header);
+// console.log(mainHeading);
+
+// const mainHeading = document.getElementById("main-heading");
+// console.log(mainHeading.textContent);
+// mainHeading.textContent = "This is something else";
+// console.log(mainHeading);
+
+//array Like object means => indexing , length property...
+
+const mainHeading = document.querySelector("div");
+console.log(mainHeading);
+mainHeading.style.color = "red";
