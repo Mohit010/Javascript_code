02ca9905907dd962e5bde1fbdf855fc10d52ddcb
Mohit <nagarmohit138@gmail.com>
2023-09-25 11:51:05 +0530

    function returning promise

---
+++ function returning promise.js
@@ -0,0 +1,17 @@
+
+function f1(){
+    return new Promise((a,b) =>{
+        const bool = true;
+        if(bool){
+            a("yes");
+        }else{
+            b("no");
+        }
+    })
+}
+
+f1().then((ans) =>{
+    console.log("The answer is " + ans);
+}).catch((error) =>{
+    console.log("The answer is " + error);
+})
